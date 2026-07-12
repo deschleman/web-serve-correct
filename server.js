076852af04580c8ecfd6048e1022ef01366c3f8f
Dante Eschleman <deschleman@umass.edu>
2026-07-12 17:04:44 +0000

    add About page

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 import express from "express";
 
 const app = express();
+app.set("view engine", "ejs"); // lab 2 new line
 const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello, web!");
 });
 
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});  // lab 2 new line
+
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
-
-// number 1  : GET/hello/:name responds: res.send("hello, {$name}!")
